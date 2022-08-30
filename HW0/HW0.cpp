997c5f54072c32dc1b01571cc67a66451cac2a53
bfforsythe <bfforsythe@alaska.edu>
2022-08-30 15:00:57 -0800

    HW0 Main program completed

---
+++ HW0/HW0.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string> 
+#include <stdio.h>
+
+
+// ------------------------------------------------------- HW0 Main Program - C to F calculator -------------------------------
+/*
+// F -> C = ( temp - 32 ) * 5/9
+double cpp_ftoc(std::string farenheit) {
+
+	auto tempCalc = std::stod(farenheit);
+	double NewTemp = (tempCalc - 32) * 0.5556;
+
+	std::cout << "Given Temperature: " << farenheit << "\n";
+	std::cout << "Converted Temp: ";
+
+
+	return(NewTemp);
+}
+
+double c_ctof(const char str[]) {
+	
+	char* end;
+
+	double celcius;
+	celcius = strtod(str, &end);
+
+	double NewTemp = (celcius *1.8) + 32;
+
+	std::cout << "Given Temperature" << celcius << "\n";
+	std::cout << "Converted Temp: ";
+
+	return(NewTemp);
+}
+
+int main(int argc, const char* argv[]) {
+
+	if (argv[1] = "--ftoc") {
+		double fheit = cpp_ftoc(argv[2]);
+		std::cout << fheit;
+	}
+	else if (argv[1] = "--ctof") {
+		double cels = c_ctof(argv[2]);
+		std::cout << cels;
+	}
+	else {
+		std::cout << "Unable to Read Input";
+	}
+
+}
+
+*/
+
+
+
+
+
+
+
+// --------------------------------------------------------- HW0 A1 Script Reader ------------------------------------------------------------------
+
+

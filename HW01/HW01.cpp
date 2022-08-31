742a60545ca90422e4f4020b67c965c985557103
bfforsythe <bfforsythe@alaska.edu>
2022-08-30 18:33:59 -0800

    Finished HW01 Main

---
+++ HW01/HW01.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <chrono>
+#include <algorithm>
+
+using namespace std::chrono;
+
+
+
+class StopWatch {
+	public:
+
+		time_point<steady_clock> start, end;
+		
+
+		void Start() {
+			start = high_resolution_clock::now();
+	}
+
+		void Stop() {
+			 end = high_resolution_clock::now();
+
+			duration<double> duration = end - start;
+			double ms = duration.count() * 1000;
+			double sec = duration.count();
+
+			std::cout << "\n" << sec << " seconds";
+			std::cout << "\n" << ms << " ms";
+		}
+
+
+};
+
+void VectorCreator(int size) {
+
+	std::vector<int> searchable;
+
+	for (int i = 0; i < size; i++) {
+		searchable.push_back(i);
+		std::cout << searchable.at(i) << "\n";
+
+			if (binary_search(searchable.begin(), searchable.end(), 9999)) {
+
+		std::cout << "Got It!";
+	}
+
+	}
+
+
+
+}
+
+
+int main() {
+	StopWatch brandDeal;
+	
+	brandDeal.Start();
+	
+	VectorCreator(10005);
+
+	brandDeal.Stop();
+}

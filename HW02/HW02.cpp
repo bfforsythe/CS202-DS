9fae22fd31fabb149f23c27be1cbdd3850425b91
bfforsythe <bfforsythe@alaska.edu>
2022-09-04 15:05:30 -0800

    HW01P1 finished

---
+++ HW02/HW02.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include "catch.hpp"
+using std::cout;
+using std::cin;
+using std::endl;
+
+// Program 1 - List Testing
+
+/*
+struct Value {
+
+	std::list<int> valList = { 1 , 10 , 5 , 9 , 13 };
+
+	void ListReset() {
+		valList = { 1, 10, 5, 9 , 13 };
+	}
+
+	void printList() {
+
+		for (auto& printer : valList) {
+			cout << printer << " ";
+		}
+
+		cout << std::endl;
+	}
+
+}Val;
+
+
+int main() {
+
+	// Queue : FIFO
+
+	cout << "FIFO demonstration : ";
+
+	Val.valList.push_back(3);
+	Val.valList.pop_front();
+
+	Val.printList();
+
+
+
+	Val.ListReset();
+
+
+	// Stack : LIFO
+
+	cout << "LIFO demonstration: ";
+	Val.valList.pop_back();
+	Val.valList.push_back(3);
+
+	Val.printList();
+
+
+
+
+	Val.ListReset();
+
+
+	// Insert & Find
+	cout << "Insert & Find Demo: ";
+	int searchTerm = 5;
+	
+
+	Val.valList.sort();
+
+	
+
+	for (std::list<int>::iterator itr_val = Val.valList.begin(); itr_val != Val.valList.end(); ++itr_val){
+		
+		if (*itr_val == searchTerm) {
+
+			dupeFound = true;
+
+			Val.valList.insert(itr_val, 3);
+			Val.valList.erase(itr_val);
+	
+			itr_val = Val.valList.begin();
+		}
+
+		
+
+	}
+	Val.printList();
+}
+
+
+*/

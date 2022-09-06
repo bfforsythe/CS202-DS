1108c802cc680f170a3992054d7e8a8ae944268a
bfforsythe <bfforsythe@alaska.edu>
2022-09-05 21:30:24 -0800

    Fixed.

--- HW03/HW03.cpp
+++ HW03/HW03.cpp
@@ -11,6 +11,8 @@ using std::endl;
 // HW03 P1
 
 /*
+
+
 int main() {
 
 	int column = 0;
@@ -36,12 +38,71 @@ int main() {
 		
 		column = column + output.length();
 			
-			cout << "Line " << line << "  " << "Column " << column << "   " << output << endl;
+			//cout << "Line " << line << "  " << "Column " << column << "   " << output << endl;
+
 		}
 
 
 
 	}
+
+	
 }
 
+
 */
+
+
+
+//HW03 P2 - Money Class (????)
+
+
+
+//HW03 A1 - Pretty Text
+
+
+int main() {
+
+	int column = 0;
+	int line = 0;
+	std::string goodline;
+
+	std::vector<std::string> paragraphs;
+
+	std::ifstream token("leagues.txt", std::fstream::in);
+
+
+	while (std::getline(token, goodline)) {
+
+		std::stringstream newStream(goodline);
+		std::string output;
+		line++;
+
+		column = 0;
+
+
+
+		while (newStream >> output) {
+
+			column = column + output.length();
+
+			//cout << "Line " << line << "  " << "Column " << column << "   " << output << endl;
+
+			paragraphs.push_back(output);
+
+			if (paragraphs.size() % 100 == 0) {
+				for (int i = 0; i < paragraphs.size(); i++) {
+					cout << paragraphs[i] << " ";
+				}
+				cout << endl;
+				paragraphs.clear();
+			}
+
+		}
+
+
+
+	}
+
+
+}

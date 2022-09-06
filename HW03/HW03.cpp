55fbfb7bc393f164ea7f66c95df7d4484e9bdffb
bfforsythe <bfforsythe@alaska.edu>
2022-09-06 13:11:48 -0800

    HW03A1 completed

--- HW03/HW03.cpp
+++ HW03/HW03.cpp
@@ -60,41 +60,58 @@ int main() {
 
 //HW03 A1 - Pretty Text
 
-
-int main() {
-
-	int column = 0;
-	int line = 0;
+void textPrinter(std::string fileName, int wrap) {
+	int periodCounter = 0;
 	std::string goodline;
-
 	std::vector<std::string> paragraphs;
+	std::ifstream token(fileName, std::fstream::in);
 
-	std::ifstream token("leagues.txt", std::fstream::in);
-
+	int size = -1;
 
 	while (std::getline(token, goodline)) {
 
 		std::stringstream newStream(goodline);
 		std::string output;
-		line++;
-
-		column = 0;
-
-
 
 		while (newStream >> output) {
 
-			column = column + output.length();
-
-			//cout << "Line " << line << "  " << "Column " << column << "   " << output << endl;
+			size++;
 
 			paragraphs.push_back(output);
 
-			if (paragraphs.size() % 100 == 0) {
-				for (int i = 0; i < paragraphs.size(); i++) {
-					cout << paragraphs[i] << " ";
-				}
+			if (size == wrap) {
 				cout << endl;
+				size = 0;
+			}
+
+
+			for (int i = 0; i < paragraphs.size(); i++) {
+				
+
+
+	
+
+				cout << paragraphs.at(i) << " ";
+
+
+
+			
+
+				if (paragraphs[i].back() == '.' || paragraphs[i].back() == '!' || paragraphs[i].back() == '?') {
+					periodCounter++;
+
+				}
+
+
+				if (periodCounter == 5) {
+
+					cout << std::endl << std::endl << std::endl;
+					periodCounter = 0;
+
+
+				} 
+
+
 				paragraphs.clear();
 			}
 
@@ -103,6 +120,13 @@ int main() {
 
 
 	}
+}
 
+int main(int argc,  char** argv) {
+
+
+	int wrapper = std::atoi(argv[2]);
+	
+	textPrinter(argv[1], wrapper);
 
 }
